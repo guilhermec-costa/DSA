d1da1baee10e153827ef8f8f3dfadc3274b2cc23
Guilherme China <echina725@gmail.com>
2025-01-25 17:43:20 -0300

    exponent

--- src/recursion.cpp
+++ src/recursion.cpp
@@ -48,6 +48,91 @@ int fun_with_static(int n)
   return 0;
 }
 
+void fnB(int n);
+
+void fnA(int n)
+{
+  if (n > 0)
+  {
+    printf("%d\n", n);
+    fnB(n - 1);
+  }
+}
+
+void fnB(int n)
+{
+  if (n > 1)
+  {
+    printf("%d\n", n);
+    fnA(n / 2);
+  }
+}
+
+int sum_loop(int n)
+{
+  int s = 0;
+  for (int i = 1; i < n + 1; i++)
+  {
+    s += i;
+  }
+
+  return s;
+}
+
+int sum_recursion(int n)
+{
+  if (n > 0)
+  {
+    return sum_recursion(n - 1) + n;
+  }
+
+  return 0;
+}
+
+int factorial_loop(int n)
+{
+  int total = 1;
+  for (int i = n; i >= 1; i--)
+  {
+    std::cout << i << "\n";
+    total *= i;
+  }
+
+  return total;
+}
+
+int factorial_recusion(int n)
+{
+  if (n <= 1)
+  {
+    return 1;
+  }
+
+  return factorial_recusion(n - 1) * n;
+}
+
+int exponent_recursion(int base, int exp)
+{
+
+  if (exp == 0)
+  {
+    return 1;
+  }
+
+  return exponent_recursion(base, exp - 1) * base;
+}
+
+int loop_exponent(int base, int exp)
+{
+  int r = 1;
+  for (int i = 0; i < exp; i++)
+  {
+    r *= base;
+  }
+
+  return r;
+}
+
 void recursion()
 {
   int n = 3;
@@ -61,4 +146,29 @@ void recursion()
   std::cout << "-----\n";
 
   printf("%d", fun_with_static(x));
+  std::cout << "-----" << std::endl;
+
+  std::cout << "-----" << std::endl;
+  int u = 5;
+  fnA(u);
+
+  const int sumLoop = sum_loop(10);
+  const int sumRecursion = sum_recursion(10);
+
+  std::cout << "Sum of 10 elements with loop: " << sumLoop << std::endl;
+  std::cout << "Sum of 10 elements with recursion: " << sumRecursion << std::endl;
+  std::cout << "-----" << std::endl;
+
+  int fact_loop = factorial_loop(5);
+  int fact_recursion = factorial_recusion(5);
+  std::cout << "Factorial of 5 with loop: " << fact_loop << std::endl;
+  std::cout << "Factorial of 5 with loop: " << fact_recursion << std::endl;
+
+  std::cout << "-----" << std::endl;
+
+  int base = 2;
+  int exponent_with_loop = loop_exponent(base, 2);
+  int recursive_exponent = exponent_recursion(base, 9);
+  std::cout << "5 to the power of 2 with loop: " << exponent_with_loop << std::endl;
+  std::cout << "5 to the power of 2 with recursion: " << recursive_exponent << std::endl;
 }
