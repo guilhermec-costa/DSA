8537fe403f1942c58cd6831453d04661ac57be22
Guilherme China <echina725@gmail.com>
2025-03-28 21:25:56 -0300

    tree includes

--- src/DT_traversals.py
+++ src/DT_traversals.py
@@ -53,6 +53,28 @@ def breadth_first_search(root_node: BinaryTreeNode) -> List[BinaryTreeNode]:
 
     return results
 
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+def tree_includes(root_node: BinaryTreeNode, target: Any) -> Union[List[BinaryTreeNode], bool]:
+    if not root_node: return False
+    queue: List[BinaryTreeNode] = [root_node]
+    while(queue.__len__() > 0):
+        cur_node = queue[0]
+        if(cur_node.data == target): return True
+        queue = remove_first(queue)
+        if(cur_node.left_node): queue.append(cur_node.left_node)
+        if(cur_node.right_node): queue.append(cur_node.right_node)
+
+    return False
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+def tree_includes_recursively(root_node: BinaryTreeNode, target: Any) -> Union[List[BinaryTreeNode], bool]:
+    if not root_node: return False
+    if(root_node.data == target): return True
+
+    return tree_includes_recursively(root_node.left_node, target) or \
+    tree_includes_recursively(root_node.right_node, target)
 
 d_node = BinaryTreeNode("D Node")
 e_node = BinaryTreeNode("E Node")
@@ -63,6 +85,8 @@ root_node = BinaryTreeNode("A Node", b_node, c_node)
 r_BFS = breadth_first_search(root_node)
 r_DFS = depth_first_search_iterative(root_node)
 r_DFS_recursive = depth_first_search_recursive(root_node)
+includes = tree_includes(root_node, "| Node")
+includes_recursion = tree_includes_recursively(root_node, "A Node")
 
 for i in r_BFS:
     print(i.data)
@@ -72,3 +96,19 @@ for i in r_DFS:
 print("------------")
 for i in r_DFS_recursive:
     print(i.data)
+print("------------")
+print(includes)
+print(includes_recursion)
+
+
+
+
+
+print("------ Tree Sum ------")
+d_node = BinaryTreeNode(4)
+e_node = BinaryTreeNode(2)
+b_node = BinaryTreeNode(11, d_node, e_node)
+f_node = BinaryTreeNode(1)
+c_node = BinaryTreeNode(4, f_node)
+root_node = BinaryTreeNode(3, b_node, c_node) 
+print(breadth_first_search(root_node))
